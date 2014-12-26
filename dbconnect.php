bb0becc02a0c6be106d39cbfb95f114e0d4f8e8c
unknown <Test2@U307C_03.stud.msk.mesi.ru>
2014-12-26 13:28:41 +0300

    Добавление всех методов для работы с бд

--- dbconnect.php
+++ dbconnect.php
@@ -5,7 +5,7 @@
 			setlocale(LC_TIME, 'ru_RU');
 			$this->mysqli = new mysqli($this->host, $this->user, $this->pass, $this->name);
 			if ($this->mysqli->connect_errno) {
-				echo "Íå óäàëîñü ïîäêëþ÷èòüñÿ ê MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error;
+				echo "ÐÐµ ÑƒÐ´Ð°Ð»Ð¾ÑÑŒ Ð¿Ð¾Ð´ÐºÐ»ÑŽÑ‡Ð¸Ñ‚ÑŒÑÑ Ðº MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error;
 			}
 		}
 
@@ -47,8 +47,70 @@
 			
 		}
 
-		
-		
+		public function getLastEdit($id) {
+			$id = (int)$id;
+			$result = $this->mysqli->prepare("SELECT * FROM history WHERE employee_id = ?");
+			$result->bind_param('d', $id);
+			
+			$result->execute();
+			$query = $result->get_result();
+			if (!is_null($query)) {
+				while ($data = $query->fetch_assoc()) {
+					$sum[] = $data;
+				}
+			} else {
+				return false;
+			}
+			
+			return $sum[0];
+		}
+
+		public function search($data) {
+
+			$data = '%'.$data.'%';
+			$data = mysqli_real_escape_string($this->mysqli, $data);
+			$result = $this->mysqli->prepare("SELECT * FROM employees WHERE name LIKE ? OR job LIKE ? OR phone LIKE ? OR email LIKE ?");
+			$result->bind_param('ssss', $data, $data, $data, $data);
+			
+			$result->execute();
+			$query = $result->get_result();
+			if (!is_null($query)) {
+				while ($data = $query->fetch_assoc()) {
+					$sum[] = $data;
+				}
+			} else {
+				return false;
+			}
+			
+			return $sum;
+		}
+
+		public function setHistory($IP, $employee_id, $date) {
+			$result = $this->mysqli->prepare("INSERT INTO history (IP, date, employee_id) VALUES (?, ?, ?)");
+			$result->bind_param('sdd', $IP, $employee_id, $date);
+			
+			$result->execute();
+			
+			return $result;
+		}
+
+		public function getHistory($id) {
+			$id = (int)$id;
+			$result = $this->mysqli->prepare("SELECT * FROM history WHERE employee_id = ? ORDER BY date DESC");
+			$result->bind_param('d', $id);
+			
+			$result->execute();
+			$query = $result->get_result();
+			if (!is_null($query)) {
+				while ($data = $query->fetch_assoc()) {
+					$sum[] = $data;
+				}
+			} else {
+				return false;
+			}
+			
+			return $sum;
+		}
 		
 		public function add($name, $job, $phone, $mail) {
 			$result = $this->mysqli->prepare("INSERT INTO employees (name, job, phone, email) VALUES (?, ?, ?, ?)");
@@ -69,7 +131,15 @@
 			return $result;
 		}
 
-		
+		public function delete($id) {
+			$id = (int)$id;
+			$result = $this->mysqli->prepare("DELETE FROM employees WHERE id = ?");
+			$result->bind_param('d', $id);
+			
+			$result->execute();
+			
+			return $result;
+		}
 	}
 
 	$dbc = new DBConnect();
